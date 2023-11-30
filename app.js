8312beeda7e1fc6068f3f30853c97d75f469119b
Rafaaaugg <rafaelmendes@alunos.utfpr.edu.br>
2023-11-30 19:40:01 -0300

    a

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 require("dotenv").config()
 
 var indexRouter = require('./routes/initF1');
-var databaseRouter = require('./routes/initF1');
+var registerRouter = require('./routes/registerAPI');
 
 var app = express();
 
@@ -11,7 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-app.use('/banco', databaseRouter);
+app.use('/register', registerRouter);
+app.use('/season', seasonRouter);
+app.use('/driver', driverRouter);
+app.use('/races', racesRouter);
 
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
